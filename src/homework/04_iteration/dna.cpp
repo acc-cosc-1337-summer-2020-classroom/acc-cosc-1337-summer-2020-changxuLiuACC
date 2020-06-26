d06a2f832228a19f62b90d035f5815ece69c3caa
changxuLiuACC <changxu.liu@g.austincc.edu>
2020-06-25 23:28:08 -0500

    Homework 3 Complete

--- src/homework/04_iteration/dna.cpp
+++ src/homework/04_iteration/dna.cpp
@@ -6,16 +6,37 @@ Calculate GC content:
 Iterate string count Gs and Cs, divide count by string length.
 Return quotient.
 */
+double get_gc_content(const string& dna)
+{
+    int GC_count{0};    //to store the GC number count (number of Gs or Cs in the DNA string)
 
+    for(char ntide: dna)    //iterates over each character of dna - in "ntide"
+    {
+        if(ntide == 'G' || ntide == 'C')
+        {
+            GC_count++;     //increases GC content count (numerator of average) if there's a G or C
+        }
+    }
 
-
+    //returns the average by dividng the GC number count by the length of the DNA (requires casting on dna.length(), it chops off the decimal)
+    return GC_count / static_cast<double>(dna.length());   
+}
 
 /*
 Write code for function get_reverse_string that
 accepts a string parameter and returns a string reversed.
 */
+string get_reverse_string(string dna)
+{
+    string backwards;   //to store the backwards of dna result
 
+    for(std::size_t i = dna.length(); i > 0; --i)    //iterates over each character of dna backwards - character in "ntide"
+    {
+        backwards.push_back(dna[i-1]);    //adds characters from dna to string variable backwards going in backwards direction
+    }   //alternatively, backwards += dna[i-1] also could have worked. 
 
+    return backwards;
+}
 
 /*
 Write prototype for function get_dna_complement that
@@ -25,6 +46,38 @@ a. call function get_reverse_string(dna), save results to a local string variabl
 b. iterate local string variable and
     replace A with T, T with A, C with G and G with C
 c. return string
-
 */
+string get_dna_complement(string dna)
+{
+    string complement = get_reverse_string(dna);  //reverses the dna and stores it in variable complement (but not done yet)
 
+    /*
+        Although it would be more effective to just do -1 to complement.length() once in the for-loop header rather than each time the index of 
+        complement is called, it won't work as after reaching the beginning value of complement (which would have been 0 for i), it'll attempt to 
+        go to -1 before checking against the condition to stop in the loop, resulting in not good stuff as size_t can't go into negative numbers. 
+    */
+
+    for(std::size_t i = complement.length(); i > 0; --i)     //iterates over each character of complement (dna reversed backwards)
+    {
+        switch(complement[i-1])     //evaluates what each character is and replaces it with it's complement
+        {
+        case 'A':
+            complement[i-1] = 'T';
+            break;
+        case 'T':
+            complement[i-1] = 'A';
+            break;
+        case 'C':
+            complement[i-1] = 'G';
+            break;
+        case 'G':
+            complement[i-1] = 'C';
+            break;
+        default:
+            return "Program incompatible with alien DNA. (Letter other than A, T, C or G detected in DNA. Please double check and try again.)";
+            break;
+        }
+    }
+
+    return complement;
+}
--- src/homework/04_iteration/main.cpp
+++ src/homework/04_iteration/main.cpp
@@ -1,7 +1,9 @@
 //write include statements
-
+#include<iostream>
+#include "dna.h"
 //write using statements
-
+using std::cout;	using std::cin;
+using std::string;
 
 /*
 Write code that prompts user to enter 1 for Get GC Content, 
@@ -12,5 +14,32 @@ user enters a y or Y.
 */
 int main() 
 {
+	int choice;		//used to store what the user wants to do
+	string dna;		//used to store the dna that is to be analyzed for GC content or complimented
+	char cont;		//used to store whter the user would like to continue additional operations in the program
+
+	do
+	{
+		cout<<"\nEnter 1 to get GC Content.\n";	
+		cout<<"Enter 2 to get DNA Complement.\n";
+		cout<<"Enter your choice: ";	//collecting user input on desired action
+		cin>>choice;
+		cout<<"Enter DNA String: ";		//collecting dna string from user for operations
+		cin>>dna;
+
+		if(choice == 1)
+		{
+			cout<<"GC Content: "<<get_gc_content(dna)<<"\n";	//calculates and prints out GC content if 1 was desired by user
+		}
+		else
+		{
+			cout<<"DNA Complement: "<<get_dna_complement(dna)<<"\n";	//creates and prints out dna complement if 2 was desired by user
+		}
+
+		cout<<"Are you done? Enter y or Y for additional DNA operations (or any other character to exit the program): ";	//prompts user if he/she would like to repeat the program
+		cin>>cont;
+	} while (cont == 'Y' || cont == 'y');	//if the user enters "y" or "Y", he/she indicates that more GC content analysis or DNA Complementing must be completed and will continue 
+											//with the loop. However, if the user enters anything else, the user indicates he/she is done and it will not continue the loop another time
+											//leading to the end of the program with the "return 0" below. 
 	return 0;
 }
